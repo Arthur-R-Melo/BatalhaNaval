79b8ac432e925bbe7a77fdca88d9ebd351ac3092
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-09-12 14:23:16 -0300

    Update Navio.java

--- BatalhaNaval/src/batalhanaval/Navio.java
+++ BatalhaNaval/src/batalhanaval/Navio.java
@@ -9,5 +9,9 @@ package batalhanaval;
  * @author 0068943
  */
 public class Navio {
+    public static final int CRUZADOR = 2, FRAGATA = 3, PORTA_AVIOES = 4;
+    
+    private int tamanho;
+    private boolean partes[];//representa se as partes do navio estao destruidas ou nao
     
 }
