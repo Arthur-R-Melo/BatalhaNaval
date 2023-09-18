e08f7521f69d0a11222a4a48a079e5b0a0914c33
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-09-18 12:00:14 -0300

    Update Navio.java

--- BatalhaNaval/src/batalhanaval/model/Navio.java
+++ BatalhaNaval/src/batalhanaval/model/Navio.java
@@ -2,16 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package batalhanaval;
+package batalhanaval.model;
 
 /**
  *
  * @author 0068943
  */
-public class Navio {
-    public static final int CRUZADOR = 2, FRAGATA = 3, PORTA_AVIOES = 4;
+public enum Navio {
+    CRUZADOR(2), FRAGATA(3), PORTA_AVIOES(4);
     
-    private int tamanho;
-    private boolean partes[];//representa se as partes do navio estao destruidas ou nao
+    private int tam;
+
+    private Navio(int tam) {
+        this.tam = tam;
+    }
+    
+    
     
 }
