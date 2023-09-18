6cad4effa0a3aeb06baf334184c24a1ba1504217
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-09-18 14:22:50 -0300

    Update Tabuleiro.java

--- BatalhaNaval/src/model/Tabuleiro.java
+++ BatalhaNaval/src/model/Tabuleiro.java
@@ -10,5 +10,27 @@ package model;
  */
 public class Tabuleiro {
     public static final int EM_BRANCO = 1, NAVIO = 2, TIRO_AGUA = 3, NAVIO_ACERTADO = 4;
+    public static final int HORIZONTAL = 1, VERTICAL = 2;
     private int tab[][];
+
+    public Tabuleiro() {
+        this.tab = new int[5][10];
+        for (int i = 0; i < this.tab.length; i++) {
+            for (int j = 0; j < this.tab[i].length; j++) {
+                this.tab[i][j] = EM_BRANCO;
+            }
+        }
+    }
+    /*
+        The String posicao must be formated as letter(a, b, c, d, e)+number(from 1 to 10) as in a1, b5, e10.
+    */
+    public boolean posicionaNavio(Navio navio, String posicao, int direcao) {
+        //TODO
+        return true;
+    }
+
+    public int[][] getTab() {
+        return tab;
+    }
+    
 }
