530402a11bdcd5f5ae4bafab88c7fd217954fed8
valtersimao <simaovalter0@gmail.com>
2023-09-21 22:28:00 -0300

    Update janela.java
    Os panels do tabuleiro agora possuem animação de clique e o posicionamento (1 - 10; A - E) foi adicionado a interface via codigo.

--- BatalhaNaval/src/view/janela.java
+++ BatalhaNaval/src/view/janela.java
@@ -1,54 +1,140 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package view;
 
-import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  *
- * @author 0068967
+ * @author Valtin
  */
 public class janela extends javax.swing.JFrame {
 
-    /**
-     * Creates new form janela
-     */
+    private JPanel[][] pecasPlayer;
+    private JPanel[][] pecasInimigo;
+
     public janela() {
         initComponents();
         config();
     }
-    
+
     private void config() {
         this.setLocationRelativeTo(null);
-        criaPanelsPecas(jTabuleiro1);
-        criaPanelsPecas(jTabuleiro2);
-        
-    }
-    
-    private JPanel[][] criaPanelsPecas(JPanel tabuleiro){
-        JPanel[][] pecas = new JPanel[10][5];
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 5; y++) {
-                pecas[x][y] = new JPanel();
-                pecas[x][y].setBackground(Color.red);
-                pecas[x][y].setSize(50,50);
-                pecas[x][y].setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-                JLabel coord = new JLabel();
-                coord.setText((y+1) + "");
-                pecas[x][y].add(coord);
-                pecas[x][y].setLayout(null);
-                coord.setSize(10,16);
-                coord.setLocation(0,0);
-                
-                tabuleiro.add(pecas[x][y]);
-                pecas[x][y].setLocation((x * 50), (y * 50));
+        this.pecasPlayer = this.criaPanelsPecas(this.jTabuleiroPlayer);
+        this.pecasInimigo = this.criaPanelsPecas(this.jTabuleiroInimigo);
+
+        //Setar todas as posicoes como clicaveis
+        for (JPanel[] vet : this.pecasPlayer) {
+            for (JPanel panel : vet) {
+                this.setPanelClicavel(panel);
             }
         }
+        for (JPanel[] vet : this.pecasInimigo) {
+            for (JPanel panel : vet) {
+                this.setPanelClicavel(panel);
+            }
+        }
+
+        escrevaCoord(this.jTabuleiroInimigo);
+        escrevaCoord(this.jTabuleiroPlayer);
+
+    }
+
+    private void escrevaCoord(JPanel referencia) {
+        int tamChar = 20;
+        char letra;
+        for (int i = 1; i <= 10; i++) {
+            //numeros
+            JLabel posicaoTxt = new JLabel();
+            posicaoTxt.setText(i + "");
+            posicaoTxt.setSize(tamChar, tamChar);
+            posicaoTxt.setForeground(new java.awt.Color(230, 230, 230));
+            this.jPanel1.add(posicaoTxt);
+
+            posicaoTxt.setLocation(((i - 1) * 50) + referencia.getX() + (25 - tamChar / 2), referencia.getY() - tamChar - 5);
+            //define a localizacao do numero paralalelamente ao Panel Tabuleiro de referencia, e calcula para manter um alinhamento
+        }
+        for (int i = 0; i < 5; i++) {
+            JLabel posicaoTxt = new JLabel();
+
+            switch (i) {
+                case 0:
+                    letra = 'A';
+                    break;
+                case 1:
+                    letra = 'B';
+                    break;
+                case 2:
+                    letra = 'C';
+                    break;
+                case 3:
+                    letra = 'D';
+                    break;
+                default:
+                    letra = 'E';
+
+            }
+            posicaoTxt.setText(letra + "");
+
+            posicaoTxt.setSize(tamChar, tamChar);
+            posicaoTxt.setForeground(new java.awt.Color(230, 230, 230));
+            this.jPanel1.add(posicaoTxt);
+
+            posicaoTxt.setLocation(referencia.getX() + referencia.getSize().width + 5, referencia.getY() + i * 50);
+        }
+    }
+
+    private JPanel[][] criaPanelsPecas(JPanel tabuleiro) {
+        JPanel[][] pecas = new JPanel[10][5];
+        for (int x = 0; x < pecas.length; x++) {
+
+            for (int y = 0; y < pecas[0].length; y++) {
+                //cria todos os panels de cada tabuleiro e inicia suas configurações iniciais
+                pecas[x][y] = new JPanel();
+                pecas[x][y].setBackground(new java.awt.Color(230, 230, 230));
+                pecas[x][y].setSize(50, 50);
+                pecas[x][y].setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+                /*JLabel coord = new JLabel();
+                coord.setText((x + 1) + "");
+                pecas[x][y].add(coord);
+                pecas[x][y].setLayout(null);
+                coord.setSize(16, 16);
+                coord.setLocation(1, 0);*/
+                tabuleiro.add(pecas[x][y]);
+                pecas[x][y].setLocation((x * 51), (y * 51));
+            }
+
+        }
         return pecas;
+
+    }
+
+    private void setPanelClicavel(JPanel panel) {
+        panel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panel.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+            }
+        });
     }
 
     /**
@@ -61,39 +147,39 @@ public class janela extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jTabuleiro1 = new javax.swing.JPanel();
-        jTabuleiro2 = new javax.swing.JPanel();
+        jTabuleiroPlayer = new javax.swing.JPanel();
+        jTabuleiroInimigo = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel1.setBackground(new java.awt.Color(75, 75, 75));
 
-        jTabuleiro1.setBackground(new java.awt.Color(0, 0, 153));
-        jTabuleiro1.setPreferredSize(new java.awt.Dimension(500, 250));
+        jTabuleiroPlayer.setBackground(new java.awt.Color(0, 0, 204));
+        jTabuleiroPlayer.setPreferredSize(new java.awt.Dimension(510, 255));
 
-        javax.swing.GroupLayout jTabuleiro1Layout = new javax.swing.GroupLayout(jTabuleiro1);
-        jTabuleiro1.setLayout(jTabuleiro1Layout);
-        jTabuleiro1Layout.setHorizontalGroup(
-            jTabuleiro1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+        javax.swing.GroupLayout jTabuleiroPlayerLayout = new javax.swing.GroupLayout(jTabuleiroPlayer);
+        jTabuleiroPlayer.setLayout(jTabuleiroPlayerLayout);
+        jTabuleiroPlayerLayout.setHorizontalGroup(
+            jTabuleiroPlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 510, Short.MAX_VALUE)
         );
-        jTabuleiro1Layout.setVerticalGroup(
-            jTabuleiro1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 250, Short.MAX_VALUE)
+        jTabuleiroPlayerLayout.setVerticalGroup(
+            jTabuleiroPlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
         );
 
-        jTabuleiro2.setBackground(new java.awt.Color(0, 0, 153));
-        jTabuleiro2.setPreferredSize(new java.awt.Dimension(500, 250));
+        jTabuleiroInimigo.setBackground(new java.awt.Color(0, 0, 204));
+        jTabuleiroInimigo.setPreferredSize(new java.awt.Dimension(510, 255));
 
-        javax.swing.GroupLayout jTabuleiro2Layout = new javax.swing.GroupLayout(jTabuleiro2);
-        jTabuleiro2.setLayout(jTabuleiro2Layout);
-        jTabuleiro2Layout.setHorizontalGroup(
-            jTabuleiro2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+        javax.swing.GroupLayout jTabuleiroInimigoLayout = new javax.swing.GroupLayout(jTabuleiroInimigo);
+        jTabuleiroInimigo.setLayout(jTabuleiroInimigoLayout);
+        jTabuleiroInimigoLayout.setHorizontalGroup(
+            jTabuleiroInimigoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 510, Short.MAX_VALUE)
         );
-        jTabuleiro2Layout.setVerticalGroup(
-            jTabuleiro2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 250, Short.MAX_VALUE)
+        jTabuleiroInimigoLayout.setVerticalGroup(
+            jTabuleiroInimigoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -101,22 +187,22 @@ public class janela extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jTabuleiro1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jTabuleiroInimigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jTabuleiro2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(294, Short.MAX_VALUE))
+                        .addComponent(jTabuleiroPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(111, 284, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jTabuleiro2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 92, Short.MAX_VALUE)
-                .addComponent(jTabuleiro1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
+                .addComponent(jTabuleiroPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                .addComponent(jTabuleiroInimigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -171,8 +257,7 @@ public class janela extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jTabuleiro1;
-    private javax.swing.JPanel jTabuleiro2;
+    private javax.swing.JPanel jTabuleiroInimigo;
+    private javax.swing.JPanel jTabuleiroPlayer;
     // End of variables declaration//GEN-END:variables
 }
